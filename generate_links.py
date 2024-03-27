b341f115af3c4ab835fc99682c49d88d232a6a92
keat <keatly91@gmail.com>
2024-03-27 13:08:24 -0400

    Updated lambda

---
+++ generate_links.py
@@ -0,0 +1,59 @@
+""" Script to generate personal survey links for a mailing list.
+"""
+
+from qualtrics_client import Client
+from pprint import pprint
+from time import sleep
+import pymysql
+import queries
+from os import environ
+
+def generate_survey_links (survey_id, survey_description, mailing_list_id, expiration_date):
+     # TODO: Pass in these values from lambda env
+    token = environ["Q_TOKEN"]
+
+    # Connect to the database
+    connection = queries.get_connection()
+
+    print("connected to database")
+    # Create client for qualtrics API
+    c = Client(api_token=token)
+
+    print("Qualtrics client created")
+    
+    # Create personal links
+    distribution_id = c.create_survey_links(survey_id, mailing_list_id, expiration_date, survey_description)
+    
+    print("Created distribution ", distribution_id)
+
+    queries.insert_distribution(connection, distribution_id, expiration_date, mailing_list_id, survey_description, survey_id)
+    sleep(2)
+    
+    print("Inserted to db")
+
+    distribution_list = update_distribution_list(c, token, distribution_id, survey_id)
+
+    print("Add distribution list ", distribution_list)
+
+    queries.insert_distribution_list_many(connection, distribution_id, expiration_date, distribution_list)
+
+    print("done generating links")
+
+
+def update_distribution_list(c, token, distribution_id, survey_id):
+    response = c.get_contact_survey_links(distribution_id, survey_id)
+
+    survey_list = []
+    for element in response:
+        vals = {
+            'contact_id': element["contactId"],
+            'distribution_id': distribution_id,
+            "survey_link": element["link"],
+            'survey_status': 'NotSent',
+            'survey_delivered': False,
+            'external_data_reference': element['externalDataReference']
+        }
+
+        survey_list.append(vals)
+        
+    return survey_list
---
+++ queries.py
@@ -0,0 +1,226 @@
+import pymysql as pms
+from datetime import datetime, timedelta
+
+from os import environ, getenv
+
+def get_connection():
+    token = environ["Q_TOKEN"]
+
+    print("Connecting to database ", environ["DB_SCHEMA"])
+    # Connect to the database
+    connection = pms.connect(host=environ["DB_HOST"],
+                                user=environ["DB_USER"],
+                                password=environ["DB_PASS"],
+                                database=environ["DB_SCHEMA"],
+                                charset='utf8mb4',
+                                cursorclass=pms.cursors.DictCursor,
+                                ssl={"fake_flag_to_enable_tls":True})
+
+    print("Connected.")
+
+    return connection
+
+def insert_distribution(conn, distribution_id, expiration_date, mailing_list, description, survey_id):
+    
+    query = """
+        INSERT INTO distribution (id, expirationDate, mailingListId, description, surveyId)
+        VALUES(%s, %s, %s, %s, %s) 
+        ON DUPLICATE KEY UPDATE expirationDate=%s, description=%s
+    """
+
+    inputs = (distribution_id, expiration_date, mailing_list, description, survey_id, expiration_date, description)
+    with conn.cursor() as cur:
+        cur.execute(query, inputs)
+        conn.commit()
+
+def insert_distribution_list_many(conn, distribution_id, expiration_date, distribution_list):
+    query = """
+        INSERT INTO distribution_list
+        (distributionId, participantId, surveyLink, surveyStatus, surveyDelivered, linkExpiration, externalDataReference)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE linkExpiration=%s, participantId=%s, surveyLink=%s
+    """
+
+    print(query)
+    inputs = [
+        (
+            distribution_id, 
+            distribution["contact_id"], 
+            distribution['survey_link'],
+            distribution['survey_status'],
+            distribution['survey_delivered'],
+            expiration_date,
+            str(distribution['external_data_reference']),
+            expiration_date,
+            distribution["contact_id"], 
+            distribution['survey_link']
+        ) 
+        for distribution in distribution_list
+    ]
+    
+    print(inputs)
+
+    with conn.cursor() as cur:
+        cur.executemany(query, inputs)
+        conn.commit()
+
+def insert_distribution_list_many_alt(conn, distribution_id, distribution_list):
+    query = """
+        INSERT INTO distribution_list
+        (distributionId, participantId, surveyLink, surveyStatus, surveyDelivered, linkExpiration, externalDataReference)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE linkExpiration=%s, participantId=%s, surveyLink=%s
+    """
+
+    with conn.cursor() as cur:
+        for distribution in distribution_list:
+            input = (
+                        distribution_id, 
+                        distribution["contact_id"], 
+                        distribution['survey_link'],
+                        distribution['survey_status'],
+                        distribution['survey_delivered'],
+                        distribution['link_expiration'],
+                        distribution['external_data_reference'],
+                        distribution['link_expiration'],
+                        distribution["contact_id"], 
+                        distribution['survey_link']
+                    ) 
+            cur.execute(query, input)
+        conn.commit()
+        
+def update_text_message_id(conn, message_id, distribution_id, contact_id):
+    query = """
+        UPDATE distribution_list
+        SET textMessageId = %s
+        WHERE distributionId = %s AND participantId = %s
+    """
+
+    inputs = (message_id, distribution_id, contact_id)
+    with conn.cursor() as cur:
+        cur.execute(query, inputs)
+
+def update_survey_delivered(conn, delivered, distribution_id, contact_id):
+    
+    query = """
+        UPDATE distribution_list
+        SET surveyDelivered = %s
+        WHERE distributionId = %s AND participantId = %s
+    """
+
+    inputs = (delivered, distribution_id, contact_id)
+    with conn.cursor() as cur:
+        cur.execute(query, inputs)
+
+def update_survey_status(conn, survey_status, distribution_id, contact_id):
+
+    query = """
+        UPDATE distribution_list
+        SET surveyStatus = %s
+        WHERE distributionId = %s AND participantId = %s
+    """
+
+    inputs = (survey_status, distribution_id, contact_id)
+    with conn.cursor() as cur:
+        cur.execute(query, inputs)
+        conn.commit()
+
+def update_survey_status_many(conn, survey_dict):
+    """ Update many contact's survey status with dict:
+
+    Exptected input:
+    {
+        "contact_id": "contactId",
+        "distribution_id": "distributionId",
+        "survey_status": 'SurveyFinished'
+    }
+
+    """
+
+    query = """
+        UPDATE distribution_list
+        SET surveyStatus = %s
+        WHERE distributionId = %s AND participantId = %s
+    """
+
+    inputs = [
+        (
+            survey["survey_status"],
+            survey["distribution_id"],
+            survey["contact_id"]
+        )
+        for survey in survey_dict
+    ]
+    with conn.cursor() as cur:
+        cur.executemany(query, inputs)
+        conn.commit()
+
+def get_distribution_by_date(conn, start_date, end_date):
+    print(start_date, end_date)
+    query = "select * from distribution where expirationDate >= %s and expirationDate < %s;" 
+    
+    inputs = (start_date, end_date)
+
+    results = []
+    with conn.cursor() as cur:
+        cur.execute(query, inputs)
+
+        results = cur.fetchall()
+
+    return results
+
+def get_distribution_by_id(conn, distribution_id):
+    query = "select * from distribution where id = %s;" 
+    
+    inputs = (distribution_id)
+
+    results = []
+    with conn.cursor() as cur:
+        cur.execute(query, inputs)
+
+        results = cur.fetchall()
+
+    return results
+
+def get_distribution_list_by_distribution_id(conn, distribution_id):
+    query = "select * from distribution_list where distributionId = %s;" 
+    
+    inputs = (distribution_id)
+
+    results = []
+    with conn.cursor() as cur:
+        cur.execute(query, inputs)
+
+        results = cur.fetchall()
+
+    return results
+
+
+def get_distribution_list_by_expiration_date(conn, expiration_date):
+    print (expiration_date)
+    query = """SELECT *
+    FROM distribution_list
+    WHERE linkExpiration >= %s AND linkExpiration <= %s;""" 
+    
+    inputs = (expiration_date, expiration_date + timedelta(days=1))
+
+    with conn.cursor() as cur:
+        cur.execute(query, inputs)
+
+        results = cur.fetchall()
+
+    return results
+    
+def get_status(conn, tomorrow):
+    query = """SELECT d.description, dl.* 
+    FROM distribution_list as dl, distribution as d
+    WHERE dl.distributionId = d.id AND dl.linkExpiration < %s
+    ORDER BY dl.participantId, dl.linkExpiration;""" 
+    
+    input = (tomorrow)
+    with conn.cursor() as cur: 
+        cur.execute(query, input)
+
+        results = cur.fetchall()
+
+    return results
